3aecc85b8b062bbff057a498666900cf8d1512dc
Fidgino <Fidgino@gmail.com>
2024-03-27 21:03:16 +0100

    first commit

---
+++ target/debug/deps/rustalgo.d
@@ -0,0 +1,5 @@
+C:\Users\Jules\OneDrive\Bureau\rustalgo\target\debug\deps\rustalgo.exe: src\main.rs
+
+C:\Users\Jules\OneDrive\Bureau\rustalgo\target\debug\deps\rustalgo.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rustalgo.d
@@ -0,0 +1,1 @@
+C:\Users\Jules\OneDrive\Bureau\rustalgo\target\debug\rustalgo.exe: C:\Users\Jules\OneDrive\Bureau\rustalgo\src\main.rs
